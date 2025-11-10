f3399d094f96ba82366a9838b71f84316cbf9ee2
ngkaizhe123 <kaizhe8229@gmail.com>
2025-11-10 12:33:29 +0800

    add info

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -12,6 +12,15 @@ public class GreetingController {
         return "form";
     }
 
+    @GetMapping("/info")
+    public String showInfo(Model model) {
+        model.addAttribute("name", "Andrew");
+        model.addAttribute("age", 20);
+        model.addAttribute("country", "Malaysia");
+        return "info"; // templates/info.html
+    }
+
+
     // process form and submit
     @PostMapping("/greet")
     public String handleForm(@RequestParam("username") String username, Model model) {
