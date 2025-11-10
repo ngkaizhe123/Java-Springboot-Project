34909e57d6cae9a4d1d6f81325c7289aeae04356
ngkaizhe123 <kaizhe8229@gmail.com>
2025-11-10 16:25:01 +0800

    add student list

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class GreetingController {
     // display form
@@ -19,6 +21,28 @@ public class GreetingController {
         model.addAttribute("country", "Malaysia");
         return "info"; // templates/info.html
     }
+
+    @GetMapping("/students")
+    public String showStudents(Model model) {
+        List<String> students = List.of("Alice", "Bob", "Charlie", "David");
+        model.addAttribute("students", students);
+        return "students"; // templates/students.html
+    }
+    //http://localhost:8080/students
+
+    @GetMapping("/studentList")
+    public String showStudentList(Model model) {
+        List<Student> students = List.of(
+                new Student("Alice", 20),
+                new Student("Bob", 21),
+                new Student("Charlie", 22),
+                new Student("David", 19)
+        );
+
+        model.addAttribute("students", students);
+        return "studentList";
+    }
+    //http://localhost:8080/studentList
 
 
     // process form and submit
